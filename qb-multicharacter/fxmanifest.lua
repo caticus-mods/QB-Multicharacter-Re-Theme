569ef4b8dea5b85cd4ff209d8edaef937ef93f68
Caticus <154236756+caticus-mods@users.noreply.github.com>
2024-03-04 10:52:36 +0000

    Add files via upload

---
+++ qb-multicharacter/fxmanifest.lua
@@ -0,0 +1,39 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+author 'Kakarot'
+description 'Allows players to create multiple characters'
+version '1.2.0'
+
+shared_scripts {
+    '@qb-core/shared/locale.lua',
+    'locales/en.lua',
+    'locales/*.lua',
+    'config.lua'
+}
+
+client_scripts {
+    'client/main.lua'
+}
+
+server_scripts {
+    '@oxmysql/lib/MySQL.lua',
+    '@qb-apartments/config.lua',
+    'server/main.lua'
+}
+
+ui_page 'html/index.html'
+
+files {
+    'html/index.html',
+    'html/style.css',
+    'html/reset.css',
+    'html/vue.js',
+    'html/swal2.js',
+    'html/profanity.js'
+}
+
+dependencies {
+    'qb-core',
+    'qb-spawn'
+}
